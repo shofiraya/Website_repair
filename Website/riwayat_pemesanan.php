c98b93518ad81c46a2b987e73eec6ba8a35abece
Mirta Chadhirotin Nachlah <129361988+mirtacn@users.noreply.github.com>
2023-07-02 23:44:44 +0700

    Update riwayat_pemesanan.php

--- Website/riwayat_pemesanan.php
+++ Website/riwayat_pemesanan.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["login_pelanggan"]) || $_SESSION["login_pelanggan"] !== tru
 }
 
 $id_pelanggan = $_SESSION['pelanggan'];
-$status = "proses";
+$status = "Proses";
 
 if (isset($_POST['pesan'])) {
     if (input($_POST) > 0) {
@@ -52,20 +52,127 @@ $data_array = mysqli_fetch_all($query, MYSQLI_ASSOC);
     <link rel="stylesheet" href="css/slicknav.min.css" />
 
     <!-- Main Stylesheets -->
-	<link rel="stylesheet" href="css/style.css" />
-
-
-
-
+    <link rel="stylesheet" href="css/style.css" />
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style>
-        
-       
 
-        
+    <style>
+    .riwayat-section p {
+        margin-bottom: 10px;
+        padding-top: 10px;
+    }
+
+    .col-3, .col-4, .col-6, .col-12 {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .riwayat-section .main {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        color: white;
+    }
+
+    .riwayat-section .card {
+        width: calc(50% - 10px);
+        /* Atur lebar card sesuai kebutuhan */
+        margin-bottom: 20px;
+        /* Atur jarak antara card */
+        background: #000000;
+        backdrop-filter: blur(2px);
+        height: 100%;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    @media (max-width: 768px) {
+        .riwayat-section .card {
+            width: 100%;
+        }
+    }
+
+    .riwayat-section h2 {
+        font-family: 'K2D', sans-serif;
+        font-size: 50px;
+        color: white;
+    }
+
+    .riwayat-section footer {
+        margin-top: 50px;
+    }
+
+    span {
+        color: white;
+    }
+
+    .riwayat-section ._tanggalJam {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #CD1818;
+    }
+
+    .riwayat-section ._26 {
+        font-family: 'K2D', sans-serif;
+        font-weight: 500;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        font-size: 40px;
+        color: white;
+    }
+
+    .riwayat-section .mei-2023 {
+        display: flex;
+        align-items: center;
+        /* Memusatkan secara vertikal */
+        justify-content: center;
+        font-size: 15px;
+        color: white;
+    }
+
+    .riwayat-section ._10-00-wib {
+        display: flex;
+        justify-content: center;
+        font-weight: 100;
+        font-size: 15px;
+        color: white;
+    }
+
+    .riwayat-section .card .col-12 {
+        text-align: left;
+    }
+
+    .riwayat-section .col-6 {
+        display: flex;
+        align-items: center;
+        /* Memusatkan secara vertikal */
+    }
+
+    .riwayat-section ._jenis {
+        font-family: 'K2D', sans-serif;
+        font-size: 30px;
+        color: #CD1818;
+    }
+
+    .riwayat-section ._detailProses {
+        display: flex;
+        align-items: center;
+        color: white;
+    }
+
+    .riwayat-section .button {
+        font-family: 'K2D', sans-serif;
+        font-weight: 500;
+        color: FFFF00;
+        background-color: #f0aa42;
+        font-size: 20px;
+        border-radius: 50px;
+        border: 4px solid #f0aa42;
+        text-align: center;
+    }
     </style>
 </head>
 
@@ -84,11 +191,11 @@ $data_array = mysqli_fetch_all($query, MYSQLI_ASSOC);
         <div class="header-right">
             <div class="user-panel">
                 <?php if (!isset($_SESSION["login_pelanggan"]) || $_SESSION["login_pelanggan"] !== true) { ?>
-                    <a href="login.php" class="hr-btn">Login</a><span>|</span>
-                    <a href="registrasi.php" class="register">Create an account</a>
+                <a href="login.php" class="hr-btn">Login</a><span>|</span>
+                <a href="registrasi.php" class="register">Create an account</a>
                 <?php } else { ?>
-                    <a href="profil.php" class="hr-btn">Profil</a><span>|</span>
-                    <a href="keluar.php" class="register">Keluar</a>
+                <a href="profil.php" class="hr-btn">Profil</a><span>|</span>
+                <a href="keluar.php" class="register">Keluar</a>
                 <?php } ?>
             </div>
         </div>
@@ -117,68 +224,68 @@ $data_array = mysqli_fetch_all($query, MYSQLI_ASSOC);
                 <?php
                 if (empty($data_array)) {
                     ?>
-                    <h5 class="_align">
-                        <?php echo "Pemesanan Belum Tersedia !"; ?>
-                    </h5>
-                    <?php
+                <h5 class="_align">
+                    <?php echo "Pemesanan Belum Tersedia !"; ?>
+                </h5>
+                <?php
                 } else {
                     foreach ($data_array as $d) {
                         ?>
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="container text-center">
-                                    <div class="row">
-                                        <div class="col-3 _tanggalJam">
-                                            <div class="container">
-                                                <div class="row row-cols-2">
-                                                    <div class="col-6 _26">
-                                                        <?php echo date("d", strtotime($d['tanggal_pesan'])); ?>
-                                                    </div>
-                                                    <div class="col-6 mei-2023">
-                                                        <?php echo date("M", strtotime($d['tanggal_pesan'])); ?><br>
-                                                        <?php echo date("Y", strtotime($d['tanggal_pesan'])); ?>
-                                                    </div>
-                                                    <div class="col-12 _10-00-wib">
-                                                        <?php echo date("H:i", strtotime($d['waktu_pesan'])); ?> WIB
-                                                    </div>
-                                                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <div class="container text-center">
+                            <div class="row">
+                                <div class="col-3 _tanggalJam">
+                                    <div class="container">
+                                        <div class="row row-cols-2">
+                                            <div class="col-6 _26">
+                                                <?php echo date("d", strtotime($d['tanggal_pesan'])); ?>
+                                            </div>
+                                            <div class="col-6 mei-2023">
+                                                <?php echo date("M", strtotime($d['tanggal_pesan'])); ?><br>
+                                                <?php echo date("Y", strtotime($d['tanggal_pesan'])); ?>
+                                            </div>
+                                            <div class="col-12 _10-00-wib">
+                                                <?php echo date("H:i", strtotime($d['waktu_pesan'])); ?> WIB
                                             </div>
                                         </div>
+                                    </div>
+                                </div>
 
-                                        <div class="col-5">
-                                            <div class="container">
-                                                <div class="row row-cols-2">
-                                                    <div class="col-12 _jenis">
-                                                        <?php echo $d['nama_elektronik']; ?>
-                                                    </div>
-                                                    <div class="col-12 _tipe">
-                                                        <?php echo $d['merk_tipe']; ?>
-                                                    </div>
-                                                </div>
+                                <div class="col-5">
+                                    <div class="container">
+                                        <div class="row row-cols-2">
+                                            <div class="col-12 _jenis">
+                                                <?php echo $d['nama_elektronik']; ?>
+                                            </div>
+                                            <div class="col-12 _tipe">
+                                                <?php echo $d['merk_tipe']; ?>
                                             </div>
                                         </div>
-                                        <div class="col-4 _detailProses">
-                                            <div class="container">
-                                                <div class="row row-cols-2">
-                                                    <div class="col-12 ">
-                                                        <div class="button">
-                                                            <?php echo $d['status']; ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12" style="text-align: end; padding: 20px;">
-                                                        Detail
-                                                        <a href="detail_pemesanan.php?id=<?php echo $d['id_pemesanan']; ?>">
-                                                            <img src="img/icons/next.png" alt=""> </a>
-                                                    </div>
-
+                                    </div>
+                                </div>
+                                <div class="col-4 _detailProses">
+                                    <div class="container">
+                                        <div class="row row-cols-2">
+                                            <div class="col-12 ">
+                                                <div class="button">
+                                                    <?php echo $d['status']; ?>
                                                 </div>
                                             </div>
+                                            <div class="col-12" style="text-align: end; padding: 20px;">
+                                                Detail
+                                                <a href="detail_pemesanan.php?id=<?php echo $d['id_pemesanan']; ?>">
+                                                    <img src="img/icons/next.png" alt=""> </a>
+                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <?php
+                    </div>
+                </div>
+                <?php
                     }
                 }
                 ?>
@@ -189,7 +296,7 @@ $data_array = mysqli_fetch_all($query, MYSQLI_ASSOC);
     </section>
     <!-- Intro section end -->
 
-    
+
     <!-- Footer section -->
     <footer class="footer-section">
         <div class="container">
@@ -215,11 +322,13 @@ $data_array = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
         </div>
         <div class="copyright">
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-			Copyright &copy;
-			<script>document.write(new Date().getFullYear());</script> by RepairLectric</a>
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-		</div>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;
+            <script>
+            document.write(new Date().getFullYear());
+            </script> by RepairLectric</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </div>
     </footer>
     <!-- Footer section end -->
 
